2607e02d35d62a43b9bc255c7d2c2d4502c364f0
Juan Pablo Salas Clavijo <111824232+JuanPaSc@users.noreply.github.com>
2025-01-23 03:55:42 -0500

    Delete prueba.py

--- src/data/prueba.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Cargar el archivo CSV
-df = pd.read_csv("./src/data/books_data.csv")
-df1 = pd.read_csv("./src/data/books_rating.csv")
-
-# Mostrar las primeras filas del DataFrame
-print(df1.head())
